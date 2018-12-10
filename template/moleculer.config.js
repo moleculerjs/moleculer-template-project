02464ac155ff1971b4729fdaa868dff29a422c8b
Icebob <mereg.norbert@gmail.com>
2018-12-10 22:28:28 +0100

    add more info

--- template/moleculer.config.js
+++ template/moleculer.config.js
@@ -4,6 +4,16 @@
  * Moleculer ServiceBroker configuration file
  * 
  * More info about options: https://moleculer.services/docs/0.13/broker.html#Broker-options
+ * 
+ * Overwrite options in production:
+ * ================================ 
+ * 	You can overwrite any option with environment variables. 
+ * 	For example to overwrite the "logLevel", use `LOGGER=warn` env var.
+ * 	To overwrite a nested parameter, e.g. retryPolicy.retries, use `RETRYPOLICY_RETRIES=10` env var.
+ *  
+ * 	To overwrite broker’s deeply nested default options, which are not presented in "moleculer.config.js", 
+ * 	via environment variables, use the `MOL_` prefix and double underscore `__` for nested properties in .env file. 
+ * 	For example, to set the cacher prefix to `MYCACHE`, you should declare an env var as `MOL_CACHER__OPTIONS__PREFIX=MYCACHE`.
  */
 module.exports = {
 	// Namespace of nodes to segment your nodes on the same network.
