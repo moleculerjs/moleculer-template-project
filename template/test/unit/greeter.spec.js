c598f8668e8004a8346bfd769ec50138ca67feff
Icebob <mereg.norbert@gmail.com>
2019-02-21 21:54:38 +0100

    disable logging in tests

--- template/test/unit/greeter.spec.js
+++ template/test/unit/greeter.spec.js
@@ -5,7 +5,7 @@ const { ValidationError } = require("moleculer").Errors;
 const TestService = require("../../services/greeter.service");
 
 describe("Test 'greeter' service", () => {
-	let broker = new ServiceBroker();
+	let broker = new ServiceBroker({ logger: false });
 	broker.createService(TestService);
 
 	beforeAll(() => broker.start());
