6bce1c3b09dab1bbc8666734cf9e5b529bdf8384
AndreMaz <andre.maz.90@gmail.com>
2022-04-03 13:35:10 +0100

    add http, socket.io and gql integration tests

--- template/services/products.service.js
+++ template/services/products.service.js
@@ -4,31 +4,31 @@ const DbMixin = require("../mixins/db.mixin");
 
 /** @type {import('moleculer').ServiceSchema} ServiceSchema Moleculer's Service Schema */
 module.exports = {
-	name: "products",
-	// version: 1
+    name: "products",
+    // version: 1
 
-	/**
-	 * Mixins
-	 */
-	mixins: [DbMixin("products")],
+    /**
+     * Mixins
+     */
+    mixins: [DbMixin("products")],
 
-	/**
-	 * Settings
-	 * @type {import('moleculer-db').DbServiceSettings}
-	 */
-	settings: {
-		// Available fields in the responses
-		fields: ["_id", "name", "quantity", "price"],
+    /**
+     * Settings
+     * @type {import('moleculer-db').DbServiceSettings}
+     */
+    settings: {
+        // Available fields in the responses
+        fields: ["_id", "name", "quantity", "price"],
 
-		// Validator for the `create` & `insert` actions.
-		entityValidator: {
-			name: "string|min:3",
-			price: "number|positive",
-		},
+        // Validator for the `create` & `insert` actions.
+        entityValidator: {
+            name: "string|min:3",
+            price: "number|positive",
+        },
 
-		// GraphQL Schema definition
-		graphql: {
-			type: `
+        // GraphQL Schema definition
+        graphql: {
+            type: `
                 """
                 This type describes a Product entity.
                 """			
@@ -59,167 +59,157 @@ module.exports = {
                     price: Int!
 				}
             `,
-		},
-	},
+        },
+    },
 
-	/**
-	 * Action Hooks
-	 */
-	hooks: {
-		before: {
-			/**
-			 * Register a before hook for the `create` action.
-			 * It sets a default value for the quantity field.
-			 *
-			 * @param {import('moleculer').Context<{quantity: Number}>} ctx
-			 */
-			create(ctx) {
-				ctx.params.quantity = 0;
-			},
-		},
-	},
+    /**
+     * Action Hooks
+     */
+    hooks: {
+        before: {
+            /**
+             * Register a before hook for the `create` action.
+             * It sets a default value for the quantity field.
+             *
+             * @param {import('moleculer').Context<{quantity: Number}>} ctx
+             */
+            create(ctx) {
+                ctx.params.quantity = 0;
+            },
+        },
+    },
 
-	/**
-	 * Actions
-	 */
-	actions: {
-		/**
-		 * The "moleculer-db" mixin registers the following actions:
-		 *  - list
-		 *  - find
-		 *  - count
-		 *  - create
-		 *  - insert
-		 *  - update
-		 *  - remove
-		 */
+    /**
+     * Actions
+     */
+    actions: {
+        /**
+         * The "moleculer-db" mixin registers the following actions:
+         *  - list
+         *  - find
+         *  - count
+         *  - create
+         *  - insert
+         *  - update
+         *  - remove
+         */
 
-		list: {
-			graphql: {
-				query: "list: Response",
-			},
-		},
-		find: {
-			graphql: {
-				query: "find: [Product]!",
-			},
-		},
-		count: {
-			graphql: {
-				query: "count: Int!",
-			},
-		},
-		create: {
-			graphql: {
-				mutation:
-					"create(name: String!, quantity: Int, price: Int): Product!",
-			},
-		},
-		insert: {
-			graphql: {
-				mutation: "insert(entity: ProductInput!): Product!",
-			},
-		},
-		update: {
-			graphql: {
-				mutation:
-					"update(id: String!, name: String!, quantity: Int, price: Int): Product!",
-			},
-		},
-		remove: {
-			graphql: {
-				mutation: "remove(id: String!): Int!",
-			},
-		},
+        list: {
+            graphql: {
+                query: "list: Response",
+            },
+        },
+        find: {
+            graphql: {
+                query: "find: [Product]!",
+            },
+        },
+        count: {
+            graphql: {
+                query: "count: Int!",
+            },
+        },
+        create: {
+            graphql: {
+                mutation: "create(name: String!, quantity: Int, price: Int): Product!",
+            },
+        },
+        insert: {
+            graphql: {
+                mutation: "insert(entity: ProductInput!): Product!",
+            },
+        },
+        update: {
+            graphql: {
+                mutation: "update(id: String!, name: String, quantity: Int, price: Int): Product!",
+            },
+        },
+        remove: {
+            graphql: {
+                mutation: "remove(id: String!): Int!",
+            },
+        },
 
-		// --- ADDITIONAL ACTIONS ---
+        // --- ADDITIONAL ACTIONS ---
 
-		/**
-		 * Increase the quantity of the product item.
-		 */
-		increaseQuantity: {
-			rest: "PUT /:id/quantity/increase",
-			params: {
-				id: "string",
-				value: "number|integer|positive",
-			},
-			graphql: {
-				mutation: "increaseQuantity(id: String!, value: Int!): Product",
-			},
-			/** @param {import('moleculer').Context<{id: String, value: Number}>} ctx */
-			async handler(ctx) {
-				const doc = await this.adapter.updateById(ctx.params.id, {
-					$inc: { quantity: ctx.params.value },
-				});
-				const json = await this.transformDocuments(
-					ctx,
-					ctx.params,
-					doc
-				);
-				await this.entityChanged("updated", json, ctx);
+        /**
+         * Increase the quantity of the product item.
+         */
+        increaseQuantity: {
+            rest: "PUT /:id/quantity/increase",
+            params: {
+                id: "string",
+                value: "number|integer|positive",
+            },
+            graphql: {
+                mutation: "increaseQuantity(id: String!, value: Int!): Product",
+            },
+            /** @param {import('moleculer').Context<{id: String, value: Number}>} ctx */
+            async handler(ctx) {
+                const doc = await this.adapter.updateById(ctx.params.id, {
+                    $inc: { quantity: ctx.params.value },
+                });
+                const json = await this.transformDocuments(ctx, ctx.params, doc);
+                await this.entityChanged("updated", json, ctx);
 
-				return json;
-			},
-		},
+                return json;
+            },
+        },
 
-		/**
-		 * Decrease the quantity of the product item.
-		 */
-		decreaseQuantity: {
-			rest: "PUT /:id/quantity/decrease",
-			params: {
-				id: "string",
-				value: "number|integer|positive",
-			},
-			graphql: {
-				mutation: "decreaseQuantity(id: String!, value: Int!): Product",
-			},
-			/** @param {import('moleculer').Context<{id: String, value: Number}>} ctx */
-			async handler(ctx) {
-				const doc = await this.adapter.updateById(ctx.params.id, {
-					$inc: { quantity: -ctx.params.value },
-				});
-				const json = await this.transformDocuments(
-					ctx,
-					ctx.params,
-					doc
-				);
-				await this.entityChanged("updated", json, ctx);
+        /**
+         * Decrease the quantity of the product item.
+         */
+        decreaseQuantity: {
+            rest: "PUT /:id/quantity/decrease",
+            params: {
+                id: "string",
+                value: "number|integer|positive",
+            },
+            graphql: {
+                mutation: "decreaseQuantity(id: String!, value: Int!): Product",
+            },
+            /** @param {import('moleculer').Context<{id: String, value: Number}>} ctx */
+            async handler(ctx) {
+                const doc = await this.adapter.updateById(ctx.params.id, {
+                    $inc: { quantity: -ctx.params.value },
+                });
+                const json = await this.transformDocuments(ctx, ctx.params, doc);
+                await this.entityChanged("updated", json, ctx);
 
-				if (json.quantity === 0) {
+                if (json.quantity === 0) {
                     this.logger.info(`Stock of ${json.name} depleted... Ordering more`);
                     // Emit a persistent event to order more products
                     // inventory.service will handle this event
-                    this.broker.emit("order.more", json);
+                    this.broker.sendToChannel("order.more", json);
                 }
 
-				return json;
-			},
-		},
-	},
+                return json;
+            },
+        },
+    },
 
-	/**
-	 * Methods
-	 */
-	methods: {
-		/**
-		 * Loading sample data to the collection.
-		 * It is called in the DB.mixin after the database
-		 * connection establishing & the collection is empty.
-		 */
-		async seedDB() {
-			await this.adapter.insertMany([
-				{ name: "Samsung Galaxy S10 Plus", quantity: 10, price: 704 },
-				{ name: "iPhone 11 Pro", quantity: 25, price: 999 },
-				{ name: "Huawei P30 Pro", quantity: 15, price: 679 },
-			]);
-		},
-	},
+    /**
+     * Methods
+     */
+    methods: {
+        /**
+         * Loading sample data to the collection.
+         * It is called in the DB.mixin after the database
+         * connection establishing & the collection is empty.
+         */
+        async seedDB() {
+            await this.adapter.insertMany([
+                { name: "Samsung Galaxy S10 Plus", quantity: 10, price: 704 },
+                { name: "iPhone 11 Pro", quantity: 25, price: 999 },
+                { name: "Huawei P30 Pro", quantity: 15, price: 679 },
+            ]);
+        },
+    },
 
-	/**
-	 * Fired after database connection establishing.
-	 */
-	async afterConnected() {
-		// await this.adapter.collection.createIndex({ name: 1 });
-	},
+    /**
+     * Fired after database connection establishing.
+     */
+    async afterConnected() {
+        // await this.adapter.collection.createIndex({ name: 1 });
+    },
 };
---
+++ template/test/integration/api.spec.js
@@ -0,0 +1,561 @@
+"use strict";
+
+process.env.PORT = 0; // Use random ports during tests
+
+const HTTPrequest = require("supertest");
+const io = require("socket.io-client");
+const { request, gql } = require("graphql-request");
+
+const { ServiceBroker } = require("moleculer");
+// Load service schemas
+const APISchema = require("../../services/api.service");
+const GreeterSchema = require("../../services/greeter.service");
+const ProductsSchema = require("../../services/products.service");
+
+describe("Test HTTP API gateway", () => {
+    let broker = new ServiceBroker({ logger: false });
+    broker.sendToChannel = jest.fn();
+
+    let greeterService = broker.createService(GreeterSchema);
+    let apiService = broker.createService(APISchema);
+    let productsService = broker.createService(ProductsSchema);
+    productsService.seedDB = null; // Disable seeding
+
+    beforeAll(() => broker.start());
+    afterAll(() => broker.stop());
+
+    let SUPER_PHONE_ID;
+    let ANOTHER_PHONE_ID;
+
+    describe('Test "greeter" endpoints', () => {
+        it("test '/api/greeter/hello'", () => {
+            return HTTPrequest(apiService.server)
+                .get("/api/greeter/hello")
+                .then(res => {
+                    expect(res.body).toEqual("Hello Moleculer");
+                });
+        });
+
+        it("test '/api/unknown-route'", () => {
+            return HTTPrequest(apiService.server)
+                .get("/api/unknown-route")
+                .then(res => {
+                    expect(res.statusCode).toBe(404);
+                });
+        });
+    });
+
+    describe('Test "products" endpoints', () => {
+        it("test '/api/products/list'", () => {
+            return HTTPrequest(apiService.server)
+                .get("/api/products/list")
+                .then(res => {
+                    expect(res.body).toEqual({
+                        page: 1,
+                        pageSize: 10,
+                        rows: [],
+                        total: 0,
+                        totalPages: 0,
+                    });
+                });
+        });
+
+        it("test '/api/products/find'", () => {
+            return HTTPrequest(apiService.server)
+                .get("/api/products/find")
+                .then(res => {
+                    expect(res.body).toEqual([]);
+                });
+        });
+
+        it("test '/api/products/count'", () => {
+            return HTTPrequest(apiService.server)
+                .get("/api/products/count")
+                .then(res => {
+                    expect(res.body).toEqual(0);
+                });
+        });
+
+        it("test '/api/products/create'", () => {
+            return HTTPrequest(apiService.server)
+                .post("/api/products/create")
+                .send({ name: "Super Phone", price: 123 })
+                .then(res => {
+                    SUPER_PHONE_ID = res.body._id;
+                    expect(res.body).toEqual({
+                        _id: expect.any(String),
+                        name: "Super Phone",
+                        price: 123,
+                        quantity: 0,
+                    });
+                });
+        });
+
+        it("test '/api/products/insert'", () => {
+            return HTTPrequest(apiService.server)
+                .post("/api/products/insert")
+                .send({ entity: { name: "Another Phone", price: 123, quantity: 10 } })
+                .then(res => {
+                    ANOTHER_PHONE_ID = res.body._id;
+                    expect(res.body).toEqual({
+                        _id: expect.any(String),
+                        name: "Another Phone",
+                        price: 123,
+                        quantity: 10,
+                    });
+                });
+        });
+
+        it("test '/api/products/update'", () => {
+            return HTTPrequest(apiService.server)
+                .post("/api/products/update")
+                .send({ id: SUPER_PHONE_ID, price: 999 })
+                .then(res => {
+                    expect(res.body).toEqual({
+                        _id: expect.any(String),
+                        name: "Super Phone",
+                        price: 999,
+                        quantity: 0,
+                    });
+                });
+        });
+
+        it("test '/api/products/remove'", () => {
+            return HTTPrequest(apiService.server)
+                .post("/api/products/remove")
+                .send({ id: SUPER_PHONE_ID })
+                .then(res => {
+                    expect(res.body).toEqual(1);
+                });
+        });
+
+        it("test '/api/products/increaseQuantity'", () => {
+            return HTTPrequest(apiService.server)
+                .post("/api/products/increaseQuantity")
+                .send({ id: ANOTHER_PHONE_ID, value: 10 })
+                .then(res => {
+                    expect(res.body).toEqual({
+                        _id: expect.any(String),
+                        name: "Another Phone",
+                        price: 123,
+                        quantity: 20,
+                    });
+                });
+        });
+
+        it("test '/api/products/decreaseQuantity'", () => {
+            return HTTPrequest(apiService.server)
+                .post("/api/products/decreaseQuantity")
+                .send({ id: ANOTHER_PHONE_ID, value: 20 })
+                .then(res => {
+                    expect(res.body).toEqual({
+                        _id: expect.any(String),
+                        name: "Another Phone",
+                        price: 123,
+                        quantity: 0,
+                    });
+                });
+        });
+    });
+});
+
+describe("Test Socket.IO API gateway", () => {
+    let broker = new ServiceBroker({ logger: false });
+    broker.sendToChannel = jest.fn();
+
+    let greeterService = broker.createService(GreeterSchema);
+    let apiService = broker.createService(APISchema);
+    let productsService = broker.createService(ProductsSchema);
+    productsService.seedDB = null; // Disable seeding
+
+    beforeAll(() => broker.start());
+    afterAll(() => broker.stop());
+
+    /**
+     * Socket.IO client helper function
+     * @param {import('socket.io-client').Socket} client
+     * @param {String} action
+     * @param {Object} params
+     * @returns
+     */
+    function callAwait(client, action, params) {
+        return new Promise(function (resolve, reject) {
+            client.emit("call", action, params, function (err, res) {
+                if (err) return reject(err);
+                resolve(res);
+            });
+        });
+    }
+
+    describe('Test "greeter" actions', () => {
+        let client;
+        let port;
+
+        beforeAll(() => {
+            port = apiService.io.httpServer.address().port;
+            client = io.connect(`ws://localhost:${port}`, { forceNew: true });
+        });
+        afterAll(() => client.disconnect());
+
+        it("test 'greeter.hello'", async () => {
+            const res = await callAwait(client, "greeter.hello");
+            expect(res).toBe("Hello Moleculer");
+        });
+
+        it("test 'greeter.welcome'", async () => {
+            const res = await callAwait(client, "greeter.welcome", { name: "Socket.IO" });
+            expect(res).toBe("Welcome, Socket.IO");
+        });
+    });
+
+    describe('Test "products" actions', () => {
+        let client;
+        let port;
+        let SUPER_PHONE_ID;
+        let ANOTHER_PHONE_ID;
+
+        beforeAll(() => {
+            port = apiService.io.httpServer.address().port;
+            client = io.connect(`ws://localhost:${port}`, { forceNew: true });
+        });
+        afterAll(() => client.disconnect());
+
+        it("test 'products.list'", async () => {
+            const res = await callAwait(client, "products.list");
+            expect(res).toEqual({ page: 1, pageSize: 10, rows: [], total: 0, totalPages: 0 });
+        });
+
+        it("test 'products.find'", async () => {
+            const res = await callAwait(client, "products.find");
+            expect(res).toEqual([]);
+        });
+
+        it("test 'products.count'", async () => {
+            const res = await callAwait(client, "products.count");
+            expect(res).toEqual(0);
+        });
+
+        it("test 'products.create'", async () => {
+            const res = await callAwait(client, "products.create", {
+                name: "Super Phone",
+                price: 123,
+            });
+            SUPER_PHONE_ID = res._id;
+
+            expect(res).toEqual({
+                _id: expect.any(String),
+                name: "Super Phone",
+                price: 123,
+                quantity: 0,
+            });
+        });
+
+        it("test 'products.insert'", async () => {
+            const res = await callAwait(client, "products.insert", {
+                entity: { name: "Another Phone", price: 123, quantity: 10 },
+            });
+            ANOTHER_PHONE_ID = res._id;
+
+            expect(res).toEqual({
+                _id: expect.any(String),
+                name: "Another Phone",
+                price: 123,
+                quantity: 10,
+            });
+        });
+
+        it("test 'products.update'", async () => {
+            const res = await callAwait(client, "products.update", {
+                id: SUPER_PHONE_ID,
+                price: 999,
+            });
+
+            expect(res).toEqual({
+                _id: expect.any(String),
+                name: "Super Phone",
+                price: 999,
+                quantity: 0,
+            });
+        });
+
+        it("test 'products.remove'", async () => {
+            const res = await callAwait(client, "products.remove", { id: SUPER_PHONE_ID });
+
+            expect(res).toEqual(1);
+        });
+
+        it("test 'products.increaseQuantity'", async () => {
+            const res = await callAwait(client, "products.increaseQuantity", {
+                id: ANOTHER_PHONE_ID,
+                value: 10,
+            });
+
+            expect(res).toEqual({
+                _id: expect.any(String),
+                name: "Another Phone",
+                price: 123,
+                quantity: 20,
+            });
+        });
+
+        it("test 'products.decreaseQuantity'", async () => {
+            const res = await callAwait(client, "products.decreaseQuantity", {
+                id: ANOTHER_PHONE_ID,
+                value: 20,
+            });
+
+            expect(res).toEqual({
+                _id: expect.any(String),
+                name: "Another Phone",
+                price: 123,
+                quantity: 0,
+            });
+        });
+    });
+});
+
+describe("Test GraphQL API gateway", () => {
+    let broker = new ServiceBroker({ logger: false });
+    broker.sendToChannel = jest.fn();
+
+    let greeterService = broker.createService(GreeterSchema);
+    let apiService = broker.createService(APISchema);
+    let productsService = broker.createService(ProductsSchema);
+    productsService.seedDB = null; // Disable seeding
+
+    beforeAll(() => broker.start());
+    afterAll(() => broker.stop());
+
+    describe('Test "greeter" actions', () => {
+        let port;
+
+        beforeAll(() => {
+            port = apiService.server.address().port;
+        });
+
+        it("test 'greeter.hello'", async () => {
+            const query = gql`
+                query {
+                    hello
+                }
+            `;
+            const res = await request(`http://localhost:${port}/graphql`, query);
+            expect(res).toEqual({ hello: "Hello Moleculer" });
+        });
+
+        it("test 'greeter.welcome'", async () => {
+            const query = gql`
+                mutation {
+                    welcome(name: "GraphQL")
+                }
+            `;
+            const res = await request(`http://localhost:${port}/graphql`, query);
+            expect(res).toEqual({
+                welcome: "Welcome, GraphQL",
+            });
+        });
+    });
+
+    describe('Test "product" actions', () => {
+        let port;
+        let SUPER_PHONE_ID;
+        let ANOTHER_PHONE_ID;
+
+        beforeAll(() => {
+            port = apiService.server.address().port;
+        });
+
+        it("test 'product.list'", async () => {
+            const query = gql`
+                query {
+                    list {
+                        total
+                        page
+                        pageSize
+                        totalPages
+                        rows {
+                            _id
+                            name
+                            quantity
+                            price
+                        }
+                    }
+                }
+            `;
+            const res = await request(`http://localhost:${port}/graphql`, query);
+            console.log(res);
+            expect(res).toEqual({
+                list: {
+                    page: 1,
+                    pageSize: 10,
+                    rows: [],
+                    total: 0,
+                    totalPages: 0,
+                },
+            });
+        });
+
+        it("test 'product.find'", async () => {
+            const query = gql`
+                query {
+                    find {
+                        _id
+                        name
+                        quantity
+                        price
+                    }
+                }
+            `;
+            const res = await request(`http://localhost:${port}/graphql`, query);
+            console.log(res);
+            expect(res).toEqual({
+                find: [],
+            });
+        });
+
+        it("test 'product.count'", async () => {
+            const query = gql`
+                query {
+                    count
+                }
+            `;
+            const res = await request(`http://localhost:${port}/graphql`, query);
+            console.log(res);
+            expect(res).toEqual({
+                count: 0,
+            });
+        });
+
+        it("test 'product.create'", async () => {
+            const query = gql`
+                mutation {
+                    create(name: "Super Phone", price: 123) {
+                        _id
+                        name
+                        quantity
+                        price
+                    }
+                }
+            `;
+            const res = await request(`http://localhost:${port}/graphql`, query);
+
+            SUPER_PHONE_ID = res.create._id;
+
+            expect(res).toEqual({
+                create: {
+                    _id: expect.any(String),
+                    name: "Super Phone",
+                    quantity: 0,
+                    price: 123,
+                },
+            });
+        });
+
+        it("test 'product.insert'", async () => {
+            const query = gql`
+                mutation {
+                    insert(entity: { name: "Another Phone", price: 123, quantity: 10 }) {
+                        _id
+                        name
+                        quantity
+                        price
+                    }
+                }
+            `;
+            const res = await request(`http://localhost:${port}/graphql`, query);
+
+            ANOTHER_PHONE_ID = res.insert._id;
+
+            expect(res).toEqual({
+                insert: {
+                    _id: expect.any(String),
+                    name: "Another Phone",
+                    price: 123,
+                    quantity: 10,
+                },
+            });
+        });
+
+        it("test 'product.update'", async () => {
+            const query = gql`
+                mutation {
+                    update(id: "${SUPER_PHONE_ID}", price: 999) {
+                        _id
+                        name
+                        quantity
+                        price
+                    }
+                }
+            `;
+            const res = await request(`http://localhost:${port}/graphql`, query);
+
+            expect(res).toEqual({
+                update: {
+                    _id: expect.any(String),
+                    name: "Super Phone",
+                    price: 999,
+                    quantity: 0,
+                },
+            });
+        });
+
+        it("test 'product.remove'", async () => {
+            const query = gql`
+                mutation {
+                    remove(id: "${SUPER_PHONE_ID}")
+                }
+            `;
+            const res = await request(`http://localhost:${port}/graphql`, query);
+
+            expect(res).toEqual({
+                remove: 1,
+            });
+        });
+
+        it("test 'product.increaseQuantity'", async () => {
+            const query = gql`
+                mutation {
+                    increaseQuantity(id: "${ANOTHER_PHONE_ID}", value: 10) {
+                        _id
+                        name
+                        quantity
+                        price
+                    }
+                }
+            `;
+            const res = await request(`http://localhost:${port}/graphql`, query);
+
+            expect(res).toEqual({
+                increaseQuantity: {
+                    _id: expect.any(String),
+                    name: "Another Phone",
+                    price: 123,
+                    quantity: 20,
+                },
+            });
+        });
+
+        it("test 'product.decreaseQuantity'", async () => {
+            const query = gql`
+                mutation {
+                    decreaseQuantity(id: "${ANOTHER_PHONE_ID}", value: 20) {
+                        _id
+                        name
+                        quantity
+                        price
+                    }
+                }
+            `;
+            const res = await request(`http://localhost:${port}/graphql`, query);
+
+            expect(res).toEqual({
+                decreaseQuantity: {
+                    _id: expect.any(String),
+                    name: "Another Phone",
+                    price: 123,
+                    quantity: 0,
+                },
+            });
+        });
+    });
+});
